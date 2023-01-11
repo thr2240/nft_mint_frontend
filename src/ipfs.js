fcb2308d74556041061378236428bbcf04b67888
thr2240 <thomasr2240@gmail.com>
2023-01-11 17:18:12 +0900

    first commit

---
+++ src/ipfs.js
@@ -0,0 +1,77 @@
+const axios = require('axios');
+const FormData = require('form-data');
+
+const key = process.env.REACT_APP_PINATA_API_KEY;
+const secret = process.env.REACT_APP_PINATA_API_SECRET;
+console.log(key);
+console.log(secret);
+
+export const uploadJSONToIPFS = async(JSONBody) => {
+    const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+    //making axios POST request to Pinata ⬇️
+    return axios 
+        .post(url, JSONBody, {
+            headers: {
+                pinata_api_key: key,
+                pinata_secret_api_key: secret,
+            }
+        })
+        .then(function (response) {
+           return {
+               success: true,
+               pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+           };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+
+    });
+};
+
+export const uploadFileToIPFS = async(file) => {
+    const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+    //making axios POST request to Pinata ⬇️
+    
+    let data = new FormData();
+    data.append('file', file);
+
+
+    return axios 
+        .post(url, data, {
+            maxBodyLength: 'Infinity',
+            headers: {
+                'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+                pinata_api_key: key,
+                pinata_secret_api_key: secret,
+            }
+        })
+        .then(function (response) {
+            console.log("image uploaded", response.data.IpfsHash)
+            return {
+               success: true,
+               pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+           };
+        })
+        .catch(function (error) {
+            console.log("error",error.message)
+            return {
+                success: false,
+                message: error.message,
+            }
+
+    });
+};
+
+
+const IPFS = {
+  uploadFile: uploadFileToIPFS,
+  uploadJSON: uploadJSONToIPFS,
+
+};
+
+export {IPFS};
+
